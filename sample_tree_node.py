22cdc80533e1c1e1dd8c318fb6f295a7e025c480
Carson Boden <cwboden@umich.edu>
2021-05-06 17:25:57 -0700

    Add MyPy annotations to SampleTreeNode

--- sample_tree_node.py
+++ sample_tree_node.py
@@ -1,37 +1,40 @@
+from typing import Callable, Generator, Sequence
+
+
 class SampleTreeNode:
-    def __init__(self, name, parent=None):
+    def __init__(self, name: str, parent: 'SampleTreeNode' = None):
         self.parent = parent
         self.samples = 0
         self.name = name
         self.sum_samples = 0
         self.children = {}
 
-    def insert(self, name, samples):
-        self.insert_internal(name.split("/"), samples)
+    def insert(self, name: str, samples: int) -> None:
+        self._insert(name.split("/"), samples)
 
-    def insert_internal(self, components, samples):
+    def _insert(self, components: Sequence[str], samples: int) -> None:
         if not components:
             self.samples += samples
         else:
             self.children.setdefault(components[0], SampleTreeNode(components[0], self))
-            self.children[components[0]].insert_internal(components[1:], samples)
+            self.children[components[0]]._insert(components[1:], samples)
         self.sum_samples += samples
 
-    def leaves(self):
+    def leaves(self) -> Generator['SampleTreeNode', 'SampleTreeNode', None]:
         if not self.children:
             yield self
         for child in self.children.values():
             for result in child.leaves():
                 yield result
 
-    def merge_up(self):
+    def _merge_up(self):
         if not self.parent:
             return self
         self.parent.samples += self.samples
         del self.parent.children[self.name]
         return self.parent
 
-    def prune_until(self, max_leaves=10, min_samples=5):
+    def prune_until(self, max_leaves: int = 10, min_samples: int = 5):
         leaves = []
         for leaf in self.leaves():
             leaves.append((leaf.samples, leaf))
@@ -42,11 +45,16 @@ class SampleTreeNode:
             lowest = heapq.heappop(leaves)
             if lowest[0] > min_samples and len(leaves) < max_leaves:
                 break
-            new_node = lowest[1].merge_up()
+            new_node = lowest[1]._merge_up()
             if len(new_node.children) == 0:
                 heapq.heappush(leaves, (new_node.samples, new_node))
 
-    def __str__(self, indent, format_samples, is_last=True):
+    def __str__(
+        self,
+        indent: str,
+        format_samples: Callable[[Sequence[str]], str],
+        is_last: bool = True
+    ) -> str:
         result = indent + (is_last and "\\---" or "+---") + self.name + ""
         if self.samples:
             result += "(%s)" % (format_samples(self.sum_samples),)
